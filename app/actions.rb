d93cd5f4544f4c417beea3647fcc31b22d775621
connor steele <connorsteele604@gmail.com>
2015-12-03 00:32:18 +0000

    ability to add homies to the database added

--- app/actions.rb
+++ app/actions.rb
@@ -3,5 +3,20 @@ get '/' do
   erb :index
 end
 get '/homies/' do
-  erb :homies
+  Homie.all.to_json
+end
+
+post '/new_homie' do
+  results = {result: false}
+  name = params[:name]
+  email = params[:email]
+  phone = params[:phone]
+
+  homie = Homie.new name: name, email: email, phone: phone
+
+  if homie.save
+    results[:result] = true
+    results[:homie_id] = homie.id
+  end
+  results.to_json
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,35 @@
-$(document).ready(function() {
+$(function() {
+  var handlers = {
+    container: $("#list-of-homies").find('tbody'),
+    addHomie: function(index, homie) {
+      var tr = $("<tr>").appendTo(tr);
+      $("<td>").text(homie.name).appendTo(tr);
+      $("<td>").text(homie.phone).appendTo(tr);
+      $("<td>").text(homie.email).appendTo(tr);
+    },
+    recieveHomies: function(homies) {
+      $.each(homies, handlers.addHomie)
+    },
+    getHomies: function() {
+      handlers.container.empty()
+      $.getJSON(homies, handlers.recieve.homies);
+    }
+  };
 
+  $("#load_homies").on('click', handlers.getHomies);
+  $("#new_homie").on('click', function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var phone = $("#phone").val();
+    var homie = {name: name, email: email, phone: phone};
+
+    $.post("/new_homie", homie, function(data){
+      if (data.result) {
+        handlers.addHomie(0, homie);
+      } else {
+        alert("unable to create a homie")
+      }
+    },'json');
+    });
+  });
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
-});
